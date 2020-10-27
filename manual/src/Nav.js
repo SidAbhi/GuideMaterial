6009a385b9e1463a02d8b31dbbc032d8a27b96df
SidAbhi <megamancodenamezerobytes@gmail.com>
2020-10-27 14:03:16 +0700

    Animated nav button

--- manual/src/Nav.js
+++ manual/src/Nav.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { animated, useSpring } from 'react-spring';
+import { animated, useSpring, to, interpolate } from 'react-spring';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from './images/Logo.svg';
 import { HashLink } from 'react-router-hash-link';
@@ -21,7 +21,25 @@ const LogoPlacement = () => {
 const Nav = () => {
     const [open, setOpen] = useState(false);
 
-    const {scale}  = useSpring({scale: open ? 100 : 1});
+    const {scale} = useSpring({scale: open ? 100 : 1});
+
+    const {svgX, svgY, svgZ} = useSpring({svgX : open ? -223.7 : 0, 
+        svgY : open ? 540 : 0, 
+        svgZ: open ? -45 : 0,
+    });
+
+    const {y} = useSpring({y : open ? 473 : 262.5});
+
+    const {svg2X, svg2Y, svg2Z} = useSpring({svg2X : open ? 540 : 0, 
+        svg2Y : open ? -223.7 : 0, 
+        svg2Z: open ? 45 : 0,
+    });
+
+    const svg = to([svgX, svgY, svgZ], (svgX, svgY, svgZ) => `translate(${svgX} ${svgY}) rotate(${svgZ})`);
+
+    const svg2 = to([svg2X, svg2Y, svg2Z], (svg2X, svg2Y, svg2Z) => `translate(${svg2X} ${svg2Y}) rotate(${svg2Z})`);
+
+    const {y2} = useSpring({y2 : open ? 473 : 683.5});
 
     useEffect(() => {
         open && (document.body.style.overflow = 'hidden');
@@ -34,8 +52,8 @@ const Nav = () => {
         </animated.div>
         <svg onClick = {() => open ? setOpen(false) : setOpen(true)} className = "navButton" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1080 1080">
                 <title>MenuIcon</title>
-                <rect x="137" y="683.5" width="806" height="134" fill="#f4f4f4"/>
-                <rect x="137" y="262.5" width="806" height="134" fill="#f4f4f4"/>
+                <animated.rect x="137" y={y} width="806" height="134" transform={svg}  fill="#f4f4f4"/>
+                <animated.rect x="137" y={y2} width="806" height="134" transform={svg2} fill="#f4f4f4"/>
         </svg>
     </div>
     
